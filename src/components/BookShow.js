517effe412f471bc55dcfe6f46ed932675e8aab7
Maria Cosmen <mcosme00@gmail.com>
2023-04-03 08:24:46 +0900

    Toggle to display edit form

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../stylesheets/book.css'
+import BookEdit from './BookEdit'
 
 const BookShow = ({book, deleteBook}) => {
+  const [showEdit, setShowEdit] = useState(false)
 
-  const getBookId = () => {
+  const handleEditBook = () => {
+    setShowEdit(!showEdit)
+  }
+
+  const handleDeleteBook = () => {
     deleteBook(book.id)
   }
+
   return (
     <div className="book" id={book.id}>
       <div className="img"></div>
       <div className="icons">
-        <img src={require('../media/icon-edit01.png')} alt="edit icon" className='edit-icon'/>
-        <img src={require('../media/icon-cancel.png')} alt="delete icon" onClick={getBookId}/>
+        <img src={require('../media/icon-edit01.png')} alt="edit icon" onClick={handleEditBook}/>
+        <img src={require('../media/icon-cancel.png')} alt="delete icon" onClick={handleDeleteBook}/>
       </div>
       <div className='book-description'>
         <h3>{book.title}</h3>
         <p>Description</p>
       </div>
+      {showEdit ? <BookEdit /> : ""}
     </div>
   )
 }
